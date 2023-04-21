2b867dfe12d907a6133a286540ab4436105a3610
NickWis <44568703+NickWis@users.noreply.github.com>
2023-04-21 11:13:31 +0300

    Add files via upload

---
+++ MeetMate/src/main/java/org/nwy/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package org.nwy.repository;
+
+import org.nwy.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ MeetMate/src/main/java/org/nwy/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.nwy.repository;
+
+import org.nwy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
